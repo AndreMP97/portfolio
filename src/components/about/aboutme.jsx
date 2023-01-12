1e2e13b0be24e4fa07fb672f63fe2878766da8a9
André Pacheco <andre.m.pacheco.97@gmail.com>
2023-01-12 15:00:13 +0000

    Tailwind scrollbar

--- src/components/about/aboutme.jsx
+++ src/components/about/aboutme.jsx
@@ -7,7 +7,7 @@ export default function AboutMe() {
                 <img className="h-48 w-48 rounded-md" src={Picture} alt="Profile" />
             </div>
             <div className="flex flex-col px-4 py-2 md:py-0 md:pr-8 md:w-3/4 text-white text-md md:text-xl font-mono space-y-2">
-                <p>Hello! My name is André Pacheco and I"m a software developer from Portugal, with a passion for web development. </p>
+                <p>Hello! My name is André Pacheco and I'm a software developer from Portugal, with a passion for web development. </p>
                 <p>I use my skills to create and maintain websites as well as web applications. My interests and expertise lie in programming languages such as JavaScript, HTML, and CSS, as well as frameworks like React. </p>
                 <p>I stay up-to-date with the latest technologies in web development to maximize the potential of my projects. My aim is to deliver high-quality results with my problem-solving and analytical skills.</p>
             </div>
