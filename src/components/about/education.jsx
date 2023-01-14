b00dc032febb6957e5fa370372be1be66b05f009
André Pacheco <andre.m.pacheco.97@gmail.com>
2023-01-14 18:46:28 +0000

    Update education.jsx

--- src/components/about/education.jsx
+++ src/components/about/education.jsx
@@ -1,4 +1,5 @@
 import Picture from "../../assets/ISEP.jfif";
+//https://www.isep.ipp.pt/Course/Course/23
 
 export default function Education() {
     return (
@@ -8,9 +9,12 @@ export default function Education() {
             </div>
             <div className="flex h-5/6 md:py-8">
                 <div className="hidden md:flex flex-col w-1/4 justify-start items-center m-2 space-y-2 text-white font-medium">
-                    <img className="md:h-32 md:w-32 lg:h-48 lg:w-48 rounded-md" src={Picture} alt="Profile" />
-                    <p>Bachelor's in Science</p>
-                    <p>2018-2022</p>
+                    <a href="https://www.isep.ipp.pt/Course/Course/23" target="_blank" rel="noreferrer">
+                        <img className="md:h-32 md:w-32 lg:h-48 lg:w-48 rounded-md hover:scale-110 duration-500" src={Picture} alt="ISEP" />
+                    </a>
+                    <p className="whitespace-nowrap">Engineering</p>
+                    <p className="whitespace-nowrap">Electrial and Computer</p>
+                    <p className="whitespace-nowrap">2018-2022</p>
                 </div>
                 <div className="flex flex-col px-4 py-2 md:py-0 md:pr-8 md:w-3/4 text-white text-md md:text-xl font-mono space-y-2">
                     <p>Education</p>
