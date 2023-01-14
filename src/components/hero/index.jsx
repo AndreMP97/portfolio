aa49bb4e3fdfc66fee4d9136c9f14a3df97fda63
André Pacheco <andre.m.pacheco.97@gmail.com>
2023-01-14 18:59:29 +0000

    added React Simple Typewriter

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -1,14 +1,21 @@
-import Picture from "../../assets/foto.jpg"
-import { FaGithub, FaLinkedin } from "react-icons/fa"
-import { ArrowDownTrayIcon } from "@heroicons/react/24/outline"
+import Picture from "../../assets/foto.jpg";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { ArrowDownTrayIcon } from "@heroicons/react/24/outline";
+import { useTypewriter } from 'react-simple-typewriter';
 
 export default function Hero() {
+    const [text] = useTypewriter({
+        words: ["Software Developer"],
+        loop: true,
+        delaySpeed: 2500
+    });
+
     return (
         <div className="bg-gray-900 flex flex-grow flex-col items-center justify-between">
             <div className="flex flex-col justify-center h-full">
                 <img className="mx-auto mt-6 h-64 w-64 rounded-full" src={Picture} alt="Profile" />
                 <h1 className="flex mt-6 mb-3 text-5xl font-bold text-white justify-center">André Pacheco</h1>
-                <h2 className="flex mb-3 text-3xl font-mono text-white justify-center">Software Developer</h2>
+                <h2 className="flex h-9 mb-3 text-3xl font-mono text-white justify-center">{text}</h2>
                 <button
                     type="button"
                     className="flex animate-pulse w-60 self-center justify-center items-center px-8 py-3 border border-transparent text-xl font-medium rounded-full shadow-sm text-white bg-gray-700 hover:bg-gray-600 hover:animate-none"
