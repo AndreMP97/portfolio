c7d989e67567b273777c18b4aacf236b20a0a3ec
André Pacheco <andre.m.pacheco.97@gmail.com>
2023-01-14 14:35:17 +0000

    changed horizontal scroll to vertical scroll

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -4,12 +4,12 @@ import Skills from "./skills";
 
 export default function About() {
     return (
-        <div className="bg-gray-900 flex flex-grow items-center justify-center">
-            <div className="flex flex-col h-5/6 w-5/6 bg-gray-800 rounded-md">
+        <div className="flex bg-gray-900 flex-grow items-center justify-center">
+            <div className="flex flex-col h-5/6 w-5/6 portrait:max-h-[600px] md:portrait:max-h-full md:landscape:max-h-[700px] bg-gray-800 rounded-md">
                 <div className="flex flex-col h-1/6 justify-center items-center">
                     <h1 className="text-white text-3xl md:text-5xl md:py-6 md:pl-8 font-serif font-semibold">About Me</h1>
                 </div>
-                <div className="flex h-5/6 overflow-x-scroll scroll-smooth snap-x snap-mandatory scrollbar-thin scrollbar-thumb-gray-700 scrollbar-thumb-rounded-full scrollbar-track-gray-600 scrollbar-track-rounded-full">
+                <div className="flex flex-wrap h-5/6 overflow-y-scroll scroll-smooth snap-y snap-mandatory scrollbar-thin scrollbar-thumb-gray-700 scrollbar-thumb-rounded-full scrollbar-track-gray-600 scrollbar-track-rounded-full">
                     <AboutMe />
                     <Education />
                     <Skills />
