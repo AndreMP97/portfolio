9987c810ac9259e7ae4ede00a4cc190628dc3848
André Pacheco <andre.m.pacheco.97@gmail.com>
2023-01-14 18:39:02 +0000

    Bug fix

--- src/components/about/skills.jsx
+++ src/components/about/skills.jsx
@@ -27,16 +27,16 @@ const icons = [
     { name: "GitHub", icon: GitHub },
 ]
 
-export default function AboutMe() {
+export default function Skills() {
     return (
         <div className="flex flex-col h-full min-w-full snap-center">
             <div className="flex flex-col h-1/6 justify-center items-center text-center">
                 <h1 className="text-white text-3xl font-semibold border-b-4 border-gray-600 md:w-[600px] md:text-5xl md:py-6">My experience</h1>
             </div>
-            <div div className="flex h-5/6 justify-center snap-center overflow-y-scroll scrollbar-none md:overflow-hidden md:flex-col">
+            <div className="flex h-5/6 justify-center snap-center overflow-y-scroll scrollbar-none md:overflow-hidden md:flex-col">
                 <div className="grid grid-cols-1 gap-2 md:gap-1 md:grid-cols-4 lg:gap-4 lg:grid-cols-6">
                     {icons.map((item) => (
-                        <div key={item.name} className="flex flex-col h-52 justify-center items-center font-mono text-center text-white hover:scale-110 duration-500 hover:animate-pulse">
+                        <div key={item.name} className="flex flex-col h-52 justify-center items-center font-mono text-center text-white hover:scale-110 duration-500">
                             <img className="w-32 md:w-24 rounded-md mx-auto" src={item.icon} alt="Profile" />
                             <p className="my-2 md:my-4">{item.name}</p>
                         </div>
