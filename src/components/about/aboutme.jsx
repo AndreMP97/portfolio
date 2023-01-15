fd5b620e0e6e85fa5ed386832ef5fcc46d32de0f
André Pacheco <andre.m.pacheco.97@gmail.com>
2023-01-15 14:28:41 +0000

    font change

--- src/components/about/aboutme.jsx
+++ src/components/about/aboutme.jsx
@@ -10,7 +10,7 @@ export default function AboutMe() {
                 <div className="hidden md:flex w-1/4 justify-center m-4">
                     <img className="md:h-32 md:w-48 lg:h-48 lg:w-64 rounded-md" src={Picture} alt="Profile" />
                 </div>
-                <div className="flex flex-col px-4 py-2 md:py-0 md:pr-8 md:w-3/4 text-white text-md md:text-xl font-mono space-y-2 overflow-auto">
+                <div className="flex flex-col px-4 py-2 md:py-0 md:pr-8 md:w-3/4 text-white text-md md:text-xl font-light space-y-2 overflow-auto">
                     <p>Hello! My name is André Pacheco and I'm a software developer from Portugal, with a passion for web development. </p>
                     <p>I use my skills to create and maintain websites as well as web applications. My interests and expertise lie in programming languages such as JavaScript, HTML, and CSS, as well as frameworks like React. </p>
                     <p>I stay up-to-date with the latest technologies in web development to maximize the potential of my projects. My aim is to deliver high-quality results with my problem-solving and analytical skills.</p>
