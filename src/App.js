bacb0f1c8b5d201d51baedd85279c17bd63aff3c
André Pacheco <andre.m.pacheco.97@gmail.com>
2023-01-15 17:15:49 +0000

    Added remaining components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import Navbar from "./components/navbar/index";
-import Hero from "./components/hero/index";
-import About from "./components/about/index";
+import Navbar from "./components/navbar/";
+import Hero from "./components/hero/";
+import About from "./components/about/";
+import Projects from "./components/projects/";
+import Contact from "./components/contact/";
+import Footer from "./components/footer";
 
 function App() {
   return (
-    <div className="h-screen overflow-y-scroll snap-y snap-mandatory lg:scrollbar scrollbar-thumb-gray-600 scrollbar-track-gray-500">
+    <div className="h-screen overflow-y-scroll snap-y snap-mandatory scrollbar-thin lg:scrollbar scrollbar-thumb-gray-600 scrollbar-track-gray-500">
       <section id="home" className="flex flex-col h-screen select-none snap-start">
         <Navbar />
         <Hero />
       </section>
       <section id="about" className="flex flex-col h-screen select-none snap-center">
         <About />
+      </section>
+      <section id="projects" className="flex flex-col h-screen select-none snap-center">
+        <Projects />
+      </section>
+      <section id="contact" className="flex flex-col h-screen select-none snap-center">
+        <Contact />
+        <Footer />
       </section>
     </div>
   );
