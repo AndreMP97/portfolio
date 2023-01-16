ddff57e0dc59a4db4704c7cec6489db3f60af857
André Pacheco <andre.m.pacheco.97@gmail.com>
2023-01-16 14:51:19 +0000

    added text to the "About me" section

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -6,11 +6,12 @@ export default function About() {
             <div className="flex flex-col h-20 md:h-32 justify-end items-center md:items-start">
                 <h1 className="text-white text-3xl font-semibold border-b-4 border-gray-600 md:text-5xl md:py-3">About me</h1>
             </div>
-            <div className="flex h-full flex-col lg:flex-row pt-8 justify-between">
+            <div className="flex h-full flex-col lg:flex-row py-8 overflow-y-scroll scrollbar-none justify-between md:overflow-y-hidden">
                 <div className="flex flex-col font-light text-white text-md space-y-2 md:text-xl lg:w-2/4">
-                    <p>Hello! My name is André Pacheco and I'm a software developer from Portugal, with a passion for web development. </p>
-                    <p>I use my skills to create and maintain websites as well as web applications. My interests and expertise lie in programming languages such as JavaScript, HTML, and CSS, as well as frameworks like React. </p>
-                    <p>I stay up-to-date with the latest technologies in web development to maximize the potential of my projects. My aim is to deliver high-quality results with my problem-solving and analytical skills.</p>
+                    <p>Hello! My name is André Pacheco and I'm a software developer from Portugal, with a passion for web development.</p>
+                    <p>I have a Bachelor's in Science (BSc) in Electrical and Computer Engineering, which combines the study of electrical engineering and computer science. The main subjects include courses in mathematics, physics, programming, electronics, instrumentation, automation, control, telecommunications, energy systems, and management.</p>
+                    <p>Overall, this degree provided me with a strong foundation in the principles and practices of electrical and computer engineering and the ability to design, develop and apply electrical and electronic systems and devices, as well as computer systems and software.</p>
+                    <p>Finally, I like to use my skills to create and maintain websites as well as web applications and I stay up-to-date with the latest technologies in web development to maximize the potential of my projects. My aim is to deliver high-quality results with my problem-solving and analytical skills.</p>
                 </div>
                 <div className="hidden md:h-full md:flex md:pt-4 lg:pt-0 lg:w-2/4 justify-center items-start">
                     <img className="h-96 rounded-md" src={Picture} alt="Profile" />
