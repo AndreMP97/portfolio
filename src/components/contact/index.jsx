1051773e7b7a474852a9da09d1dc8b051009c10f
André Pacheco <andre.m.pacheco.97@gmail.com>
2023-01-16 12:24:58 +0000

    fix autofill style in Chrome based browsers

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -12,14 +12,30 @@ export default function Contact() {
             <div className="flex h-full pt-8 justify-center items-center text-white text-lg">
                 <div className="flex h-full w-4/5 lg:w-3/5">
                     <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col h-full w-full space-y-2 md:space-y-4">
-                        <input {...register("firstName", { required: true })} aria-invalid={errors.firstName ? "true" : "false"} placeholder="First Name" className="w-full text-white border-b-2 bg-gray-900 focus:outline-none" />
+                        <input {...register("firstName", { required: true })} 
+                        aria-invalid={errors.firstName ? "true" : "false"} 
+                        placeholder="First Name" 
+                        className="w-full text-white border-b-2 bg-gray-900 focus:outline-none" />
                         {errors.firstName?.type === 'required' && <small className="text-red-600">First name is required</small>}
-                        <input {...register("lastName", { required: true })} aria-invalid={errors.lastName ? "true" : "false"} placeholder="Last Name" className="w-full text-white border-b-2 bg-gray-900 focus:outline-none" />
+                        
+                        <input {...register("lastName", { required: true })} 
+                        aria-invalid={errors.lastName ? "true" : "false"} 
+                        placeholder="Last Name" 
+                        className="w-full text-white border-b-2 bg-gray-900 focus:outline-none" />
                         {errors.lastName?.type === 'required' && <small className="text-red-600">Last name is required</small>}
-                        <input {...register("email", { required: true })} aria-invalid={errors.email ? "true" : "false"} placeholder="Email" className="w-full text-white border-b-2 bg-gray-900 focus:outline-none" />
+
+                        <input {...register("email", { required: true })} 
+                        aria-invalid={errors.email ? "true" : "false"} 
+                        placeholder="Email" 
+                        className="w-full text-white border-b-2 bg-gray-900 focus:outline-none" />
                         {errors.email?.type === 'required' && <small className="text-red-600">Email is required</small>}
-                        <textarea {...register("message", { required: true })} aria-invalid={errors.message ? "true" : "false"} placeholder="Your message" className="resize-none h-1/2 w-full text-white border-b-2 bg-gray-900 focus:outline-none" />
+                        
+                        <textarea {...register("message", { required: true })} 
+                        aria-invalid={errors.message ? "true" : "false"} 
+                        placeholder="Your message" 
+                        className="resize-none h-1/2 w-full text-white border-b-2 bg-gray-900 focus:outline-none" />
                         {errors.message?.type === 'required' && <small className="text-red-600">Message is required</small>}
+                        
                         <input type="submit" className="h-10 w-full bg-gray-800 text-white text-bold rounded-md hover:bg-gray-600" />
                     </form>
                 </div>
