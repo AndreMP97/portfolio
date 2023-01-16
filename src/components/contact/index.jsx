2ab9090edc338f3bee4114b119638f4e98a288f9
André Pacheco <andre.m.pacheco.97@gmail.com>
2023-01-16 11:55:41 +0000

    added react-hook-form to contact form

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -1,4 +1,8 @@
+import { useForm } from "react-hook-form";
+
 export default function Contact() {
+    const { register } = useForm();
+
     return (
         <div className="flex flex-col h-full w-5/6">
             <div className="flex flex-col h-20 md:h-32 justify-end items-center md:items-start">
@@ -6,12 +10,12 @@ export default function Contact() {
             </div>
             <div className="flex h-full pt-8 justify-center items-center text-white text-lg">
                 <div className="flex h-full w-4/5 lg:w-3/5">
-                    <form className="h-full w-full space-y-2 md:space-y-4">
-                        <input type="text" id="fname" name="fname" placeholder="First Name" className="w-full text-white border-b-2 bg-gray-900 focus:outline-none" /><br />
-                        <input type="text" id="lname" name="lname" placeholder="Last Name" className="w-full text-white border-b-2 bg-gray-900 focus:outline-none" /><br />
-                        <input type="text" id="email" name="email" placeholder="Email" className="w-full text-white border-b-2 bg-gray-900 focus:outline-none" /><br />
-                        <textarea id="message" name="message" placeholder="Your message" className="resize-none h-1/2 w-full text-white border-b-2 bg-gray-900 focus:outline-none" />
-                        <button className="h-12 w-full bg-gray-800 text-white text-bold rounded-md hover:bg-gray-600">Submit</button>
+                    <form onSubmit={ () => console.log("sent")} className="h-full w-full space-y-2 md:space-y-4">
+                        <input {...register("firstName", { required: true })} placeholder="First Name" className="w-full text-white border-b-2 bg-gray-900 focus:outline-none"/>
+                        <input {...register("lastName", { required: true })} placeholder="Last Name" className="w-full text-white border-b-2 bg-gray-900 focus:outline-none"/>
+                        <input {...register("email", { required: true })} placeholder="Email" className="w-full text-white border-b-2 bg-gray-900 focus:outline-none"/>
+                        <textarea {...register("message", { required: true })} placeholder="Your message" className="resize-none h-1/2 w-full text-white border-b-2 bg-gray-900 focus:outline-none" />
+                        <input type="submit" className="h-12 w-full bg-gray-800 text-white text-bold rounded-md hover:bg-gray-600" />
                     </form>
                 </div>
             </div>
