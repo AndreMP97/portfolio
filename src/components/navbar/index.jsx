c2553144e53f67e60b621cd761c4b0fecae39f59
André Pacheco <andre.m.pacheco.97@gmail.com>
2023-01-17 17:27:51 +0000

    navbar redesign

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
-import { Bars3Icon, XMarkIcon, UserIcon, BookOpenIcon, CubeIcon, InboxIcon } from "@heroicons/react/24/outline";
-import Logo from "../../assets/portfolio.svg";
+import { Bars3Icon, XMarkIcon, UserIcon, HomeIcon, CubeIcon, InboxIcon } from "@heroicons/react/24/outline";
 
 const navigation = [
-    { name: "About", href: "#about", current: false, icon: <UserIcon className="block h-10 w-10 mr-1 md:h-8 md:w-8" /> },
-    { name: "Experience", href: "#skills", current: false, icon: <BookOpenIcon className="block h-10 w-10 mr-1 md:h-8 md:w-8" /> },
-    { name: "Projects", href: "#projects", current: false, icon: <CubeIcon className="block h-10 w-10 mr-1 md:h-8 md:w-8" /> },
-    { name: "Contact", href: "#contact", current: false, icon: <InboxIcon className="block h-10 w-10 mr-1 md:h-8 md:w-8" /> },
+    { name: "Home", href: "#home", current: true, icon: <HomeIcon className="block h-10 w-10 mr-1 md:h-4 md:w-4 lg:h-6 lg:w-6" /> },
+    { name: "About", href: "#about", current: false, icon: <UserIcon className="block h-10 w-10 mr-1 md:h-4 md:w-4 lg:h-6 lg:w-6" /> },
+    { name: "Projects", href: "#projects", current: false, icon: <CubeIcon className="block h-10 w-10 mr-1 md:h-4 md:w-4 lg:h-6 lg:w-6" /> },
+    { name: "Contact", href: "#contact", current: false, icon: <InboxIcon className="block h-10 w-10 mr-1 md:h-4 md:w-4 lg:h-6 lg:w-6" /> },
 ]
 
 /*function classNames(...classes) {
@@ -28,22 +27,22 @@ export default function Navbar() {
     const [nav, setNav] = useState(false);
 
     return (
-        <nav className="flex shrink-0 justify-center items-center w-full h-20 top-0 bg-gray-900 z-40">
+        <nav className="flex shrink-0 justify-center items-center h-20 w-full top-0 z-40">
             <div className="flex w-5/6 justify-between">
                 <div className="flex">
-                    <img
-                        className="h-8 w-auto"
-                        src={Logo}
-                        alt="Logo"
-                    />
-                    <h1 className="self-center text-xl font-semibold text-white pl-2">Portfolio</h1>
+                    <h1 className="self-center text-3xl font-display text-white -rotate-12">AP</h1>
                 </div>
 
                 {/*Desktop*/}
-                <ul className="hidden md:flex items-center space-x-6">
+                <ul className="hidden md:flex justify-center items-center space-x-6 w-full">
                     {navigation.map((item) => (
                         <li key={item.name}
-                            className="text-white hover:text-gray-300 hover:scale-110 duration-500 text-base cursor-pointer capitalize font-semibold"
+                            className={
+                                item.current ?
+                                    "text-white border-b-2 py-1 hover:text-gray-300 hover:scale-110 duration-500 text-xs lg:text-sm cursor-pointer capitalize font-semibold"
+                                    :
+                                    "text-white hover:text-gray-300 hover:scale-110 duration-500 text-xs lg:text-sm cursor-pointer capitalize font-semibold"
+                            }
                         >
                             <a href={item.href} className="flex items-center">
                                 {item.icon}
@@ -53,6 +52,15 @@ export default function Navbar() {
                     ))}
                 </ul>
 
+                <div className="flex">
+                    <button
+                        type="button"
+                        className="inline-flex px-4 justify-center items-center border border-transparent text-xs lg:text-sm font-medium rounded-md shadow-sm text-white bg-gray-700 hover:bg-gray-600 hover:animate-none"
+                    >
+                        Resume
+                    </button>
+                </div>
+
                 {/* Mobile */}
                 <div className="md:hidden z-40" onClick={() => setNav(!nav)}>
                     {nav ? <XMarkIcon className="text-white block h-6 w-6" /> : <Bars3Icon className="text-white block h-6 w-6" />}
@@ -61,7 +69,7 @@ export default function Navbar() {
                     <ul className="md:hidden absolute top-0 left-0 flex flex-col justify-center items-center w-full h-screen backdrop-blur-3xl space-y-4">
                         {navigation.map((item) => (
                             <li key={item.name}
-                                className= "text-white px-4 py-6 text-4xl capitalize font-medium"
+                                className="text-white px-4 py-6 text-4xl capitalize font-medium"
                             >
                                 <a href={item.href} onClick={() => setNav(!nav)} className="flex">
                                     {item.icon}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,10 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ["Great Vibes", "cursive"],
+    },
+    extend: {}
   },
   plugins: [
     require('tailwind-scrollbar')
