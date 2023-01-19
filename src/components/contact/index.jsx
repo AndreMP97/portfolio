4034dbe65fd85a53bcd431f8e304d489614b8a1f
André Pacheco <andre.m.pacheco.97@gmail.com>
2023-01-19 16:22:37 +0000

    contact page new prototype

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -1,5 +1,5 @@
 import { useForm } from "react-hook-form";
-import Sidenav from "../sidenav";
+import { FaLocationArrow, FaRegEnvelope, FaPhoneAlt } from "react-icons/fa";
 
 export default function Contact() {
     const { register, formState: { errors }, handleSubmit, trigger } = useForm();
@@ -14,19 +14,24 @@ export default function Contact() {
     };
 
     return (
-        <div className="flex flex-col h-full w-5/6">
-            <div className="flex w-full">
-                <div className="flex flex-col h-20 w-1/2 justify-end items-start md:h-32 md:w-4/6">
-                    <h1 className="text-white text-3xl font-semibold border-b-4 border-gray-600 md:text-5xl md:py-3">Contact me</h1>
-                </div>
-                <div className="flex justify-end items-end w-1/2 md:h-32 md:w-2/6">
-                    <Sidenav
-                    current={"Contact"} 
-                    />
-                </div>
+        <div className="flex flex-col h-5/6 w-5/6 mx-auto">
+            <div className="flex flex-col h-20 justify-end items-start">
+                <h1 className="text-white text-3xl font-semibold border-b-4 border-gray-600 md:text-5xl md:py-3">Contact me</h1>
             </div>
-            <div className="flex h-full mt-4 justify-center items-center text-white text-lg">
-                <div className="flex h-full w-4/5 lg:w-3/5">
+            <div className="flex flex-col h-full pt-4 justify-center items-center pb-4">
+                <div className="flex flex-col h-1/3 w-full lg:w-2/5 justify-center space-y-4">
+                    <div className="flex space-x-4">
+                        <FaLocationArrow size={32} className="text-white" />
+                        <p className="text-white text-lg">Porto, Portugal</p>
+                    </div>
+                    <div className="flex space-x-4">
+                        <FaRegEnvelope size={32} className="text-white" />
+                        <a href="mailto:andre.m.pacheco.97@gmail.com" rel="noreferrer" className="text-white text-lg">
+                            andre.m.pacheco.97@gmail.com
+                        </a>
+                    </div>
+                </div>
+                <div className="flex h-2/3 w-full lg:w-2/5 text-white text-lg">
                     <form
                         onSubmit={handleSubmit(onSubmit)}
                         id="contactForm"
@@ -37,29 +42,29 @@ export default function Contact() {
                         <input {...register("firstName", { required: true, maxLength: 128 })}
                             aria-invalid={errors.firstName ? "true" : "false"}
                             placeholder="First Name"
-                            className="w-full text-white border-b-2 bg-gray-900 focus:outline-none" />
+                            className="w-full text-white border-b-2 bg-inherit focus:outline-none" />
                         {errors.firstName?.type === "required" && <small className="text-red-600">First name is required</small>}
 
                         <input {...register("lastName", { required: true, maxLength: 128 })}
                             aria-invalid={errors.lastName ? "true" : "false"}
                             placeholder="Last Name"
-                            className="w-full text-white border-b-2 bg-gray-900 focus:outline-none" />
+                            className="w-full text-white border-b-2 bg-inherit focus:outline-none" />
                         {errors.lastName?.type === "required" && <small className="text-red-600">Last name is required</small>}
 
                         <input {...register("email", { required: true, pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i })}
                             aria-invalid={errors.email ? "true" : "false"}
                             placeholder="Email"
-                            className="w-full text-white border-b-2 bg-gray-900 focus:outline-none" />
+                            className="w-full text-white border-b-2 bg-inherit focus:outline-none" />
                         {errors.email?.type === "required" && <small className="text-red-600">Email is required</small>}
                         {errors.email?.type === "pattern" && <small className="text-red-600">Invalid email address.</small>}
 
                         <textarea {...register("message", { required: true })}
                             aria-invalid={errors.message ? "true" : "false"}
                             placeholder="Your message"
-                            className="resize-none h-full w-full text-white border-b-2 bg-gray-900 focus:outline-none" />
+                            className="resize-none h-full w-full text-white border-b-2 bg-inherit focus:outline-none" />
                         {errors.message?.type === "required" && <small className="text-red-600">Message is required</small>}
 
-                        <button type="submit" className="h-16 w-full bg-gray-800 text-white text-bold rounded-md hover:bg-gray-600">Submit</button>
+                        <button type="submit" className="h-16 w-full bg-white text-gray-800 text-bold rounded-md hover:bg-gray-600">Submit</button>
                     </form>
                 </div>
             </div>
