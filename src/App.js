c91d74ba7d5c9fd75612b670cad044ba565ffa56
André Pacheco <andre.m.pacheco.97@gmail.com>
2023-01-20 15:15:19 +0000

    added loader component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import Countdown from "react-countdown";
+import Loader from "./components/loader/";
 import Navbar from "./components/navbar/";
 import Hero from "./components/hero/";
 import About from "./components/about/";
@@ -5,28 +7,43 @@ import Projects from "./components/projects/";
 import Contact from "./components/contact/";
 import Footer from "./components/footer";
 
+const renderer = ({ completed }) => {
+  if (completed) {
+    // Render the page when loader is done
+    return (
+      <div className="h-screen overflow-y-scroll scroll-smooth snap-y snap-mandatory scrollbar-thin lg:scrollbar scrollbar-thumb-gray-600 scrollbar-track-gray-500 bg-gray-900">
+          <nav className="container flex mx-auto sticky top-0 z-40 justify-center items-center h-0">
+            <Navbar />
+          </nav>
+          <section id="home" className="container flex h-screen mx-auto snap-start bg-gray-800 select-none items-center">
+            <Hero />
+          </section>
+          <section id="about" className="container flex h-screen mx-auto snap-start bg-gray-800 select-none items-center">
+            <About />
+          </section>
+          <section id="projects" className="container flex h-screen mx-auto snap-start bg-gray-800 select-none items-center">
+            <Projects />
+          </section>
+          <section id="contact" className="container flex h-screen mx-auto snap-start bg-gray-800 select-none items-center">
+            <Contact />
+          </section>
+          <footer className="container flex mx-auto z-40 justify-center items-center h-0">
+            <Footer />
+          </footer>
+      </div>
+    );
+  } else {
+    // Render the loader component
+    return <Loader />;
+  }
+};
+
 function App() {
   return (
-    <div className="h-screen overflow-y-scroll scroll-smooth snap-y snap-mandatory scrollbar-thin lg:scrollbar scrollbar-thumb-gray-600 scrollbar-track-gray-500 bg-gray-900">
-      <nav className="container flex mx-auto sticky top-0 z-40 justify-center items-center h-0">
-        <Navbar />
-      </nav>
-      <section id="home" className="container flex h-screen mx-auto snap-start bg-gray-800 select-none items-center">
-        <Hero />
-      </section>
-      <section id="about" className="container flex h-screen mx-auto snap-start bg-gray-800 select-none items-center">
-        <About />
-      </section>
-      <section id="projects" className="container flex h-screen mx-auto snap-start bg-gray-800 select-none items-center">
-        <Projects />
-      </section>
-      <section id="contact" className="container flex h-screen mx-auto snap-start bg-gray-800 select-none items-center">
-        <Contact />
-      </section>
-      <footer className="container flex mx-auto z-40 justify-center items-center h-0">
-        <Footer />
-      </footer>
-    </div>
+    <Countdown
+      date={Date.now() + 3000}
+      renderer={renderer}
+    />
   );
 }
 
---
+++ src/components/loader/index.jsx
@@ -0,0 +1,9 @@
+export default function Loader() {
+    return (
+        <div className="h-screen bg-gray-900">
+            <div className="container flex h-full justify-center items-center bg-gray-800 mx-auto">
+                <h1 className="animate-bounce text-9xl font-display text-white -rotate-12">AP</h1>
+            </div>
+        </div>
+    )
+}
