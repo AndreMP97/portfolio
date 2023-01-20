def26299d38ae715edde3cc611d548a6b91280a3
André Pacheco <andre.m.pacheco.97@gmail.com>
2023-01-20 16:17:59 +0000

    changed footer style

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,17 +1,14 @@
-import { FaRegCopyright } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
 export default function Footer() {
     return (
-        <div className="flex w-5/6 justify-between mb-20">
-            <div className="flex">
-                <FaRegCopyright className="h-6 w-auto text-white" />
-                <h1 className="self-center text-medium font-normal text-white pl-2">2023 André Pacheco</h1>
-            </div>
-            <div className="text-white hover:text-gray-300 text-sm cursor-pointer capitalize font-medium">
-                <a href="#test" className="flex">
-                    Disclaimer
-                </a>
-            </div>
+        <div className="flex w-5/6 justify-center items-center mb-20">
+            <div></div>
+            <a href="https://github.com/AndreMP97/portfolio" target="_blank" rel="noreferrer" className="flex flex-col text-white hover:text-gray-400">
+                <h1 className="self-center text-medium font-normal pl-2">Designed and built by André Pacheco</h1>
+                <FaGithub className="h-6 w-auto" />
+            </a>
+            <div></div>
         </div>
     )
 }
