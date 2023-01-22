520732930695d44419368fe41551304c37994ab1
André Pacheco <andre.m.pacheco.97@gmail.com>
2023-01-22 14:52:20 +0000

    about me card new style

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,19 +1,24 @@
 import Skills from "./skills";
+import Cube from "../../assets/cube.png";
 
 export default function About() {
     return (
         <div className="flex flex-col h-5/6 w-5/6 mx-auto">
             <div className="flex flex-col h-16 md:h-20 justify-end items-start">
-                <h1 className="text-white text-3xl font-semibold border-b-4 border-gray-600 md:text-5xl md:py-3">About me</h1>
+                <h1 className="text-white text-3xl font-semibold border-b-4 border-gray-600 md:text-4xl lg:text-5xl md:py-3">About me</h1>
             </div>
-            <div className="flex h-full flex-col my-4 overflow-y-scroll scrollbar-none md:overflow-y-hidden space-y-4 lg:flex-row lg:space-y-0 lg:space-x-4">
-                <div className="flex flex-col space-y-2 lg:w-2/4">
-                    <p className="font-light text-white text-md md:text-lg xl:text-xl">I have a Bachelor's in Science (BSc) in Electrical and Computer Engineering, which combines the study of electrical engineering and computer science. The main subjects include courses in mathematics, physics, programming, electronics, instrumentation, automation, control, telecommunications, energy systems, and management.</p>
-                    <p className="font-light text-white text-md md:text-lg xl:text-xl">Overall, this degree provided me with a strong foundation in the principles and practices of electrical and computer engineering and the ability to design, develop and apply electrical and electronic systems and devices, as well as computer systems and software.</p>
-                    <p className="font-light text-white text-md md:text-lg xl:text-xl">Finally, I like to use my skills to create and maintain websites as well as web applications and I stay up-to-date with the latest technologies in web development to maximize the potential of my projects. My aim is to deliver high-quality results with my problem-solving and analytical skills.</p>
+            <div className="flex flex-col h-full my-4 overflow-y-scroll scrollbar-none md:overflow-y-hidden lg:flex-row lg:space-x-4 select-text">
+                <div className="flex flex-col h-full space-y-2 lg:space-y-4 lg:w-7/12 xl:w-full">
+                    <p className="font-light text-white text-base md:text-lg 2xl:text-xl">I'm André Pacheco, a software developer living in Portugal. I enjoy creating applications that can be used by everyone on the internet. My interest in software development started at a young age when I decided to inspect the elements of a website, which led me to learn the basics of HTML and CSS.</p>
+                    <p className="font-light text-white text-base md:text-lg 2xl:text-xl">Fast forwarding to 2018, I enrolled in a Bachelor of Science in Electrical and Computer Engineering, which combines the study of electrical engineering and computer science. During my degree, I became familiar with a multitude of subjects such as mathematics, physics, programming, electronics, instrumentation, automation, control, telecommunications, energy systems, and management.</p>
+                    <p className="font-light text-white text-base md:text-lg 2xl:text-xl">Today, with the subjects that I learned during my B.Sc., I have the ability to design and develop software. Furthermore, I'm an avid learner who enjoys experimenting with new technologies during my free time, and I'm constantly striving to improve my skills to stay up-to-date with the industry.</p>
+                    <div className="flex flex-col h-full lg:hidden xl:flex">
+                        <p className="font-semibold text-white text-base md:text-lg 2xl:text-xl mb-4">Technologies I've worked with:</p>
+                        <Skills />
+                    </div>
                 </div>
-                <div className="flex flex-col space-y-4 lg:w-2/4">
-                    <p className="font-semibold text-white text-md md:text-lg xl:text-xl">Technologies I've worked with:</p>
+                <div className="hidden lg:flex flex-col space-y-4 lg:w-5/12 xl:hidden">
+                    <p className="font-semibold text-white text-base md:text-lg xl:text-xl">Technologies I have worked with:</p>
                     <Skills />
                 </div>
             </div>
--- src/components/about/skills.jsx
+++ src/components/about/skills.jsx
@@ -28,11 +28,11 @@ const icons = [
 
 export default function Skills() {
     return (
-        <div className="grid grid-cols-2 gap-4 md:grid-cols-4 md:gap-0 lg:grid-cols-6 lg:gap-4 h-32">
+        <div className="grid grid-cols-3 gap-4 md:portrait:grid-cols-4 md:landscape:grid-cols-3 md:gap-2 lg:gap-y-8 xl:grid-cols-6 xl:gap-4 h-32">
             {icons.map((item) => (
                 <div key={item.name} className="flex flex-col justify-between items-center hover:scale-110 duration-500">
-                    <img className="w-16 rounded-md m-auto" src={item.icon} alt="Profile" />
-                    <p className="font-light text-white">{item.name}</p>
+                    <img className="w-12 md:w-14 2xl:w-16 rounded-md m-auto" src={item.icon} alt="Profile" />
+                    <p className="font-light text-white text-sm 2xl:text-base">{item.name}</p>
                 </div>
             ))}
         </div>
