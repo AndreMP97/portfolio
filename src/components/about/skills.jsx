bfd437fc4a3014ac9b4e09b1e3e86e194a4aee38
André Pacheco <andre.m.pacheco.97@gmail.com>
2023-01-24 13:37:27 +0000

    changed icons to library

--- src/components/about/skills.jsx
+++ src/components/about/skills.jsx
@@ -1,37 +1,39 @@
-import Bootstrap from "../../assets/bootstrap-icon.png";
-import Cpp from "../../assets/cpp-icon.png";
-import CSS from "../../assets/css-icon.png";
-import GitHub from "../../assets/github-icon.png";
-import HTML from "../../assets/html-icon.png";
-import Java from "../../assets/java-icon.png";
-import JS from "../../assets/javascript-icon.png";
-import MySQL from "../../assets/mysql-icon.png";
-import PHP from "../../assets/php-icon.png";
-import Python from "../../assets/python-icon.png";
-import React from "../../assets/react-js-icon.png";
-import Tailwind from "../../assets/tailwind-icon.png";
+import { 
+    SiBootstrap, 
+    SiCplusplus,
+    SiCss3,
+    SiGithub,
+    SiHtml5,
+    SiJava,
+    SiJavascript,
+    SiMysql,
+    SiPhp,
+    SiPython,
+    SiReact,
+    SiTailwindcss,
+} from "react-icons/si";
 
 const icons = [
-    { name: "HTML", icon: HTML },
-    { name: "CSS", icon: CSS },
-    { name: "Bootstrap", icon: Bootstrap },
-    { name: "Tailwind", icon: Tailwind },
-    { name: "JavaScript", icon: JS },
-    { name: "React", icon: React },
-    { name: "MySQL", icon: MySQL },
-    { name: "PHP", icon: PHP },
-    { name: "Python", icon: Python },
-    { name: "C/C++", icon: Cpp },
-    { name: "Java", icon: Java },
-    { name: "GitHub", icon: GitHub },
+    { name: "HTML", icon: <SiHtml5 size={64} className="m-auto text-gray-400" /> },
+    { name: "CSS", icon: <SiCss3 size={64} className="m-auto text-gray-400" /> },
+    { name: "Bootstrap", icon: <SiBootstrap size={64} className="m-auto text-gray-400" /> },
+    { name: "Tailwind", icon: <SiTailwindcss size={64} className="m-auto text-gray-400" /> },
+    { name: "JavaScript", icon: <SiJavascript size={64} className="m-auto text-gray-400" /> },
+    { name: "React", icon: <SiReact size={64} className="m-auto text-gray-400" /> },
+    { name: "MySQL", icon: <SiMysql size={64} className="m-auto text-gray-400" /> },
+    { name: "PHP", icon: <SiPhp size={64} className="m-auto text-gray-400" /> },
+    { name: "Python", icon: <SiPython size={64} className="m-auto text-gray-400" /> },
+    { name: "C/C++", icon: <SiCplusplus size={64} className="m-auto text-gray-400" /> },
+    { name: "Java", icon: <SiJava size={64} className="m-auto text-gray-400" /> },
+    { name: "GitHub", icon: <SiGithub size={64} className="m-auto text-gray-400" /> },
 ]
 
 export default function Skills() {
     return (
         <div className="grid grid-cols-3 gap-4 md:portrait:grid-cols-4 md:landscape:grid-cols-3 md:gap-2 lg:gap-y-8 xl:gap-4 2xl:grid-cols-6 h-32">
             {icons.map((item) => (
-                <div key={item.name} className="flex flex-col justify-between items-center hover:scale-110 duration-500">
-                    <img className="w-12 md:w-14 2xl:w-16 rounded-md m-auto" src={item.icon} alt="Profile" />
+                <div key={item.name} className="flex flex-col justify-between items-center space-y-2">
+                    {item.icon}
                     <p className="font-light text-white text-sm 2xl:text-base">{item.name}</p>
                 </div>
             ))}
