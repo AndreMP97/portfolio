5838810aaba47d4dac07fb0e1a49eba8a28a8207
André Pacheco <andre.m.pacheco.97@gmail.com>
2023-01-24 14:19:38 +0000

    fixed fonts

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <div className="flex w-5/6 justify-center items-center mb-20">
             <div></div>
             <a href="https://github.com/AndreMP97/portfolio" target="_blank" rel="noreferrer" className="flex flex-col text-white hover:text-gray-400">
-                <h1 className="self-center text-medium font-normal pl-2">Designed and built by André Pacheco</h1>
+                <h4 className="font-semibold">Designed and built by André Pacheco</h4>
                 <FaGithub className="h-6 w-auto" />
             </a>
             <div></div>
