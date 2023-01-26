4add82588e42b7679c2c75762845c86c1ded2a71
André Pacheco <andre.m.pacheco.97@gmail.com>
2023-01-26 16:50:29 +0000

    added transition animation to footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,14 +1,30 @@
 import { FaGithub } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 export default function Footer() {
     return (
-        <div className="flex h-24 w-5/6 justify-center items-center my-2 md:my-4">
+        <motion.div
+            initial={{
+                opacity: 0
+            }}
+            transition={{
+                duration: 1
+            }}
+            whileInView={{
+                y: 0,
+                opacity: 1
+            }}
+            viewport={{
+                once: true,
+                amount: 0.4
+            }}
+            className="flex h-24 w-5/6 justify-center items-center my-2 md:my-4">
             <a href="https://github.com/AndreMP97/portfolio" target="_blank" rel="noreferrer" className="">
                 <div className="flex flex-col text-white hover:text-gray-400 space-y-4 md:space-y-6">
                     <h4 className="font-semibold">Designed and built by André Pacheco</h4>
                     <FaGithub className="h-6 w-auto" />
                 </div>
             </a>
-        </div>
+        </motion.div>
     )
 }
